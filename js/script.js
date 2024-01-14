2aa30b2d436f39a825f5d9c5c80d48bebc275e4e
Aleks <aleks33305@mail.ru>
2024-01-14 15:01:54 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+console.log('arr' + "- object");
+console.log(4 +  + "5");
+
+let incr = 10,
+    decr = 10;
+
+   //++ incr;
+   //-- decr;
+    
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2*8 == 8);
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Ivan";
+
+//alert(`Привет, ${user}`);
+
+
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = + prompt("Вам есть 18?", "18"); 
+//console.log(answer + 5 );
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваше фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+//let number = 4.6;
+
+//console.log(-4/0);
+//console.log('string' * 9);
+//const persone = `5`;
+
+//const bool = false;
+
+//console.log(something);
+
+//let und;
+//console.log(und);
+
+//const obj = {
+   // name: "John",
+   // age: 25,
+   // isMarried: false
+//};
+
+//console.log(obj.name);
+//console.log(obj["name"]);
+
+
+//let arr = ['plum.png','orang.jpg', 6, 'apple.bmp', {}, []];
+//console.log(arr[1]);
+
+
+
+
