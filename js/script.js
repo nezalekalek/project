fdb8d32e6c9318cfede75ef779a2c6e9672c0863
Aleks <aleks33305@mail.ru>
2024-01-17 18:48:41 +0400

    script.js mod

--- js/script.js
+++ js/script.js
@@ -38,6 +38,11 @@ const personalMoviDB = {
       b = prompt('На сколько оцените его?', ''),
       c = prompt('Один из последних просмотренных фильмов?', ''),
       d = prompt('На сколько оцените его?', '');
+personalMoviDB.movies[a] = b;
+personalMoviDB.movies[c] = d;
+
+
+console.log(personalMoviDB);
    
 
 
